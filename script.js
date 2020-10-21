65a38cb193b52a0ad682b32f08e6ccc28bbf9f0f
Andrey1303110 <andrey1303110@gmail.com>
2020-10-21 11:43:34 +0300

    add image and svg

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeItem() {
+    document.getElementById('.up div:nth-child(1)').style.zIndex = '2';
+}// функция, которая сработает при наведении.
+//она означает - выбрать элемент к Id у которого надо что-то изменить.
+// когда в скобки где написано one-two добавите Id своего элемента
+function rechangeItem() {
+    document.getElementById('.dw div:nth-child(1)').style.zIndex = '1';
+}// тут всё также. но наобарот. протсес происходящий про отводе курсора.
